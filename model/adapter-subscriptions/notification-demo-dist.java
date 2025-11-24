1fa7e0203791bfad7129c6192940b13de6643c62
richardritz <richardritz@users.noreply.github.com>
2025-11-24 10:11:39 +0000

    Add notification-demo-dist.java, dist-service-advanced.txt

---
+++ model/adapter-subscriptions/notification-demo-dist.java
@@ -0,0 +1,3 @@
+// Auto-generated file: /Users/kevinguoquan/Desktop/zama备用文件/测试/Zama项目/crowdtask_z/model/adapter-subscriptions/notification-demo-dist.java
+// Generated at: 2025-12-03T23:45:45.412Z
+
